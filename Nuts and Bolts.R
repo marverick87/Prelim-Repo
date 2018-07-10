decda1a7fe5da3280ce7794ecb9c3750588e2df0
marverick87 <marverick7788@yahoo.com>
2018-07-10 16:10:14 +0800

    Activity

---
+++ Nuts and Bolts.R
@@ -0,0 +1,83 @@
+x<-1
+print(x)
+x
+>msg<- "hello"
+> msg <- "hello"
+x <- 1:20
+x
+y <-[5:10]
+print(y)
+y <-5:10
+print(y)
+z = x+y
+a<-1.3L
+a<-1
+a
+a<-1.34
+a
+X <-0:6
+>class(x)
+X<- 0:6
+>class(x)
+X<- 0:6
+> class(x)
+>x<- 0:6
+>class(x)
+> x <- 0:6
+> class(x)
+x <- 0:6
+class(x)
+as.numeric(x)
+as.logical(x)
+as.character(x)
+as.numeric(X)
+x<- c("a", "b", "c")
+as.numeric(X)
+z<- c("a", "b", "c")
+as.numeric(X)
+z<- c("a", "b", "c")
+as.numeric(Z)
+z<- c("a", "b", "c")
+as.numeric(z)
+a<- c("a", "b", "c")
+as.numeric(a)
+m<- matrix(nrow = 2,ncol = 3)
+m
+>dim(m)
+m<- matrix(nrow = 2,ncol = 3)
+m
+>dim(m)
+dim(m)
+attributes(m)
+m<- matrix(1:6, nrow = 2,ncol = 3)
+m<- matrix(1:6, nrow = 2,ncol = 3)
+m
+m<- 1:10
+m
+dim(m)<- c(2,5)
+m
+x<- 1;3
+y<- 10:12
+cbind(x, y)
+rbind(x, y)
+x<- factor(c("yes","yes", "no", "yes","no"))
+x
+x<- factor(c("yes","yes", "no", "yes","no"))
+x
+table(x)
+unclass(x)
+x<- factor(c("yes", "yes", "no", "yes", "no")levels = c("yes", "no"))
+x
+x<- factor(c("yes", "yes", "no", "yes", "no"),levels = c("yes", "no"))
+x
+x<- c("a", "b", "c")
+as.numeric(x)
+x<- c("a", "b", "c")
+x
+x<- c("a", "b", "c")
+x
+as.numeric(x)
+as.logical(x)
+as.complex(x)
+x<- list(1, "a", TRUE, 1 + 4i)
+x
